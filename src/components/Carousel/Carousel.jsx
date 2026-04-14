c4e95097160ac09b464d560da3e0ef9c994f9091
Mudit-09 <muditsingh0911@gmail.com>
2026-04-14 19:58:09 +0530

    fix carousel navigation

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,34 +3,24 @@ import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 
-import LeftNav from "./LeftNav";
-import RightNav from "./RightNav";
-
 function Carousel({ children }) {
   return (
-    <div style={{ position: "relative" }}>
-      <Swiper
-        modules={[Navigation]}
-        spaceBetween={20}
-        navigation={{
-          nextEl: ".right-btn",
-          prevEl: ".left-btn",
-        }}
-        breakpoints={{
-          320: { slidesPerView: 2 },
-          768: { slidesPerView: 4 },
-          1024: { slidesPerView: 6 },
-        }}
-      >
-        {children.map((child, index) => (
-          <SwiperSlide key={index}>{child}</SwiperSlide>
-        ))}
-      </Swiper>
-
-      {/* Buttons */}
-      <LeftNav />
-      <RightNav />
-    </div>
+    <Swiper
+      modules={[Navigation]}
+      navigation
+      spaceBetween={20}
+      slidesPerView={6}   // 🔥 IMPORTANT (default)
+      slidesPerGroup={1}  // 🔥 CRITICAL FIX
+      breakpoints={{
+        320: { slidesPerView: 2 },
+        768: { slidesPerView: 4 },
+        1024: { slidesPerView: 6 },
+      }}
+    >
+      {children.map((child, index) => (
+        <SwiperSlide key={index}>{child}</SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
 
